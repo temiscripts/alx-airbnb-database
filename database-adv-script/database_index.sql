65cb908408853bc3a64441dcd1bdf0d56cc7fc44
temiscripts <mokhefue@gmail.com>
2025-09-07 12:54:46 +0100

    add explain analyze

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,14 +1,28 @@
+-- Measure query performance BEFORE indexing
+EXPLAIN ANALYZE
+SELECT booking.id, users.first_name
+FROM booking
+INNER JOIN users
+ON booking.user_id = users.id;
+
 -- Create index on Users table for email (often used in WHERE clause)
 CREATE INDEX idx_users_email ON users(email);
 
 -- Create index on Bookings table for user_id (used in JOIN and filtering by user)
-CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX idx_bookings_user_id ON booking(user_id);
 
 -- Create index on Bookings table for property_id (used in JOIN and filtering by property)
-CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+CREATE INDEX idx_bookings_property_id ON booking(property_id);
 
 -- Create index on Properties table for location (if filtering by city/location)
-CREATE INDEX idx_properties_location ON properties(location);
+CREATE INDEX idx_properties_location ON property(location);
 
--- Create index on Bookings table for booking_date (if used for sorting or filtering by date)
-CREATE INDEX idx_bookings_booking_date ON bookings(booking_date);
+-- Create index on Bookings table for start_date (if used for sorting or filtering by date)
+CREATE INDEX idx_bookings_start_date ON booking(start_date);
+
+-- Measure query performance AFTER indexing
+EXPLAIN ANALYZE
+SELECT booking.id, users.first_name
+FROM booking
+INNER JOIN users
+ON booking.user_id = users.id;
